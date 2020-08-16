7c7cff2eabddeb89ec27ad9d093f98290311d150
methoil <mateev.br@gmail.com>
2020-08-16 18:50:55 -0400

    feat - add redux sagas for task creating

--- src/app/components/dashboard.jsx
+++ src/app/components/dashboard.jsx
@@ -6,7 +6,7 @@ export const Dashboard = ({groups}) => (
     <div>
         <h2>Connected Dashboard</h2>
         {groups.map(group => (
-            <ConnectedTaskList id={group.id} name={group.name}></ConnectedTaskList>
+            <ConnectedTaskList key={group.id} id={group.id} name={group.name}></ConnectedTaskList>
         ))}
     </div>
 )
