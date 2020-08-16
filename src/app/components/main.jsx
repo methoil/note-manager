98d245f0338d6d87e3b965b42d942d34260c8eae
methoil <mateev.br@gmail.com>
2020-08-16 18:23:31 -0400

    chore - add react-router-dom
    - depenency and add into code

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import {Provider } from 'react-redux';
-import { store } from '../store/store';
-import { ConnectedDashboard } from './dashboard';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
+import { history } from "../store/history";
+import { ConnectedDashboard } from "./dashboard";
+import { Router, Route } from "react-router-dom";
+import { ConnectedNavigation } from "./navigation";
 
 export const Main = () => (
+  <Router history={history}>
     <Provider store={store}>
-        <div>
-            <ConnectedDashboard></ConnectedDashboard>
-        </div>
+      <div>
+        {/* <ConnectedDashboard></ConnectedDashboard> */}
+        <ConnectedNavigation/>
+        <Route
+          exact
+          path="/dashboard"
+          render={() => <ConnectedDashboard />}
+        ></Route>
+      </div>
     </Provider>
+  </Router>
 );
